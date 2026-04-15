98d20e1ab14209c2b28f15c8b3f519f52956c659
Dominik Schullerer <89839452+DominikSchullerer@users.noreply.github.com>
2026-04-15 06:07:35 +0200

    Add dummy DisplayPanel

--- src/View/Chip8EmuView.java
+++ src/View/Chip8EmuView.java
@@ -5,10 +5,13 @@ import java.awt.*;
 
 public class Chip8EmuView {
     JFrame frame;
+    DisplayPanel displayPanel;
 
     public Chip8EmuView() {
         frame = new JFrame("Chip8 Emulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        displayPanel = new DisplayPanel();
+        frame.getContentPane().add(displayPanel, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
     }
---
+++ src/View/DisplayPanel.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DisplayPanel extends JPanel {
+    public DisplayPanel() {
+        this.setPreferredSize(new Dimension(640, 320));
+        this.setBackground(new Color(0, 32, 0));
+    }
+}
