debaba673f5877c4e24aaa11587390e8913e242e
Dominik Schullerer <89839452+DominikSchullerer@users.noreply.github.com>
2026-04-15 19:40:25 +0200

    Add a dummy menu to window

--- src/View/Chip8EmuView.java
+++ src/View/Chip8EmuView.java
@@ -6,12 +6,18 @@ import java.awt.*;
 public class Chip8EmuView {
     JFrame frame;
     DisplayPanel displayPanel;
+    Chip8EmuMenu menu;
 
     public Chip8EmuView() {
         frame = new JFrame("Chip8 Emulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         displayPanel = new DisplayPanel();
         frame.getContentPane().add(displayPanel, BorderLayout.CENTER);
+
+        menu = new Chip8EmuMenu();
+        frame.setJMenuBar(menu.getMenuBar());
+
         frame.pack();
         frame.setVisible(true);
     }
